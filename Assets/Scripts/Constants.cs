88ccf59f37110a162a687c03788b31a9ef82ae98
Agustín Fernández Chávez <agustinfernandezchavezc@gmail.com>
2022-09-13 11:40:47 -0300

    Ship movement with speed limit

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,13 @@
+﻿public static class Constants
+{
+    public static class Ship
+    {
+        public const float DefaultMovementSpeed = 8f;
+        public const float MaxSpeed = 10f;
+    }
+
+    public static class Axes
+    {
+        public const string Horizontal = "Horizontal";
+    }
+}
---
+++ Assets/Scripts/ShipController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class ShipController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    private float maxSpeed;
+
+    [SerializeField]
+    private float movementSpeed;
+
+    private float MovementSpeed { 
+        get
+        {
+            if(movementSpeed > maxSpeed)
+            {
+                return maxSpeed;
+            }
+            return movementSpeed;
+        } 
+    }
+
+    private void LimitMoveSpeed()
+    {
+        if (rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * (maxSpeed - ((Vector2)movement * MovementSpeed).magnitude);
+        }
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        maxSpeed = Constants.Ship.MaxSpeed;
+        movementSpeed = Constants.Ship.DefaultMovementSpeed;
+    }
+
+    private void Update()
+    {
+        float moveHorizontal = Input.GetAxis(Constants.Axes.Horizontal);
+        movement = new Vector2(moveHorizontal, 0f);
+    }
+
+    private void FixedUpdate()
+    {
+        LimitMoveSpeed();
+        rb.AddForce(movement * MovementSpeed);
+    }
+}
